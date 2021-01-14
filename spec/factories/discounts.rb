288d07fedc65b9af1bf825a9b644aed635d61f24
Ely Hess <Ely.hess@me.com>
2021-01-14 16:08:08 -0700

    Add discount factory

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :discount do
-    name { "MyString" }
-    percentage { 1 }
-    quantity_threshold { 1 }
-    merchant { nil }
+    name { Faker::Marketing.buzzwords + " discount" }
+    percentage { [10, 20, 50].sample }
+    quantity_threshold { [5, 10, 15].sample }
   end
 end
