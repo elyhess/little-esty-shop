cc07f567ce99d4abcc642218b649611442e4e6d6
Ely Hess <Ely.hess@me.com>
2021-01-14 16:08:30 -0700

    Add discount show spec

--- spec/features/merchant/discounts/show_spec.rb
+++ spec/features/merchant/discounts/show_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
+include ActionView::Helpers::NumberHelper
 
-RSpec.describe 'description' do
-  describe 'description' do
-    it 'does something' do
-      
-    end
-  end
+RSpec.describe 'As a merchant' do
+	describe 'when i visit the merchant discounts show page' do
+		before :each do
+			@user = create(:user, role: 0)
+			@merchant = create(:merchant, user: @user)
+
+			@discount2 = create(:discount, merchant: @merchant)
+			@discount3 = create(:discount, merchant: @merchant)
+			@discount4 = create(:discount, merchant: @merchant)
+			@discount1 = create(:discount, merchant: @merchant)
+			@discount5 = create(:discount, merchant: @merchant)
+
+			login_as(@user, scope: :user)
+		end
+
+		it ' i see all the discounts for that merchant' do
+			visit merchant_discount_path(@merchant, @discount1)
+
+			expect(page).to have_content(@discount1.name)
+			expect(page).to have_content(@discount1.quantity_threshold)
+			expect(page).to have_content(number_to_percentage(@discount1.percentage, strip_insignificant_zeros: true))
+		end
+	end
 end
