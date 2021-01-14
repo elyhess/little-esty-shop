e68ce664d5d16332d415a49fb44872b6af83d45e
Ely Hess <Ely.hess@me.com>
2021-01-14 14:20:10 -0700

    Add discount model & migrations

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe Discount, type: :model do
+describe 'validations' do
+	# it { should validate_presence_of : }
+end
+
+describe 'relationships' do
+	it { should belong_to :merchant }
+end
+
+describe 'instance methods' do
+end
+
+describe 'class methods' do
+end
+
+end
+
