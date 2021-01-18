3d9ddb7fc56fd98c6a3a828867d25023301f92ea
Ely Hess <Ely.hess@me.com>
2021-01-17 19:23:10 -0700

    Update total_revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,8 +8,6 @@ class Invoice < ApplicationRecord
   enum status: [ :cancelled, :in_progress, :completed ]
 
   def total_revenue
-    # invoice_items.sum(:unit_price)
-    # # write test for this
     invoice_items.sum("invoice_items.unit_price * invoice_items.quantity")
   end
 
