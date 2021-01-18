6f908888f28054917eb60cda8acf54046d4e2ba5
Ely Hess <Ely.hess@me.com>
2021-01-17 19:25:09 -0700

    Update factories

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :bulk_discount do
     name { Faker::Marketing.buzzwords + " discount" }
-    percentage { [10, 20, 50].sample }
-    quantity_threshold { [5, 10, 15].sample }
+    percentage { [10, 20, 30].sample }
+    quantity_threshold { [2, 3, 5].sample }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
+  i = 0
   factory :user do
-    email { Faker::Internet.email }
+    email { "merchant#{i += 1}@example.com"}
     password { "strongpassword" }
     password_confirmation { "strongpassword" }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     user_name { Faker::Ancient.titan }
-
   end
 end
