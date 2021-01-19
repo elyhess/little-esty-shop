d6996bda6d960c85879761f0eeadcb54af9fb828
Ely Hess <Ely.hess@me.com>
2021-01-18 22:37:29 -0700

    unfinished orders & payments

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,13 @@ class OrdersController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def create
+    # 1
     @order = Order.new(cart.contents, current_user.customer)
     redirect_to order_path(@order)
   end
 
   def show
-    require "pry"; binding.pry
+    # 2
     @order = Order.new(cart.contents, current_user.customer)
   end
 
