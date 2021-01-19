bf9e947acd0314ccaf64a9f83c6e8b9505bb35c2
Ely Hess <Ely.hess@me.com>
2021-01-19 16:39:23 -0700

    Add braintree configuration

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,5 +1,14 @@
 
-Braintree::Configuration.environment = Rails.env.development? ? ENV['BRAINTREE_ENVIRONMENT'] : :development
+# Braintree::Configuration.environment = Rails.env.development? ? ENV['BRAINTREE_ENVIRONMENT'] : :development
+
+if Rails.env.development?
+	Braintree::Configuration.environment = :sandbox
+elsif Rails.env.production?
+	Braintree::Configuration.environment = :sandbox
+else
+	Braintree::Configuration.environment = :development
+end
+
 Braintree::Configuration.logger = Logger.new('log/braintree.log')
 Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
 Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
