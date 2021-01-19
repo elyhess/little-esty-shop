1997623014fb42ed7fc970b2f8ea6362d9db0a9f
Ely Hess <Ely.hess@me.com>
2021-01-19 11:48:51 -0700

    Initial commit

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,5 @@
-Braintree::Configuration.environment = Rails.env.production? ? :production : :sandbox
-Braintree::Configuration.merchant_id = "66gmcnn7h4crgpy7"
-Braintree::Configuration.public_key = "3xgc4hxthdvg7qxx"
-Braintree::Configuration.private_key = "68961e26e68d8c581272860de9fbe2c9"
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.logger = Logger.new('log/braintree.log')
+Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
